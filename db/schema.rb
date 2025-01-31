629a4be56eaecfa440067249b30779340589aee8
stkisengese <skisengese@outlook.com>
2025-01-31 18:43:58 +0300

    feat: update set database product table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_01_30_121457) do
+ActiveRecord::Schema[8.0].define(version: 2025_01_31_153247) do
+  create_table "products", force: :cascade do |t|
+    t.string "title"
+    t.decimal "price"
+    t.string "model"
+    t.text "description"
+    t.string "brand"
+    t.string "color"
+    t.string "condition"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_products_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -23,4 +37,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_01_30_121457) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "products", "users"
 end
