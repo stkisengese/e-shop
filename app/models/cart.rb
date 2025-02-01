c960a73d669b855d02b70eb7dc10752ae30ab7f2
stkisengese <skisengese@outlook.com>
2025-02-01 16:46:21 +0300

    feat: add cart CRUD actions

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,17 @@
 class Cart < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, optional: true
+  has_many :line_items, dependent: :destroy
+
+  # Add items to the cart
+  def add_product(product)
+    line_item = line_items.find_or_initialize_by(product: product)
+    line_item.quantity ||= 0
+    line_item.quantity += 1
+    line_item.save
+  end
+
+  # Calculate the total price of items in the cart
+  def total_price
+    line_items.sum { |item| item.product.price * item.quantity }
+  end
 end
