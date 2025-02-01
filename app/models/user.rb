9a1f8cbbf7066ddc6dd7789040ba1067c7b2ffd7
stkisengese <skisengese@outlook.com>
2025-02-01 16:47:18 +0300

    feat: add association with the cart

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,18 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
+  # Devise modules for authentication
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # Add association with products
+  has_many :products, dependent: :destroy
+
+  # Add association with the cart
+  has_one :cart, dependent: :destroy
 
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
 
-  # Add association with products
-  has_many :products, dependent: :destroy
 end
