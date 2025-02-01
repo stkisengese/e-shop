e0215608cd2c3334e46dad772af7987df3f8e150
stkisengese <skisengese@outlook.com>
2025-02-01 17:09:19 +0300

    feat: add cart items database attributes

--- db/migrate/20250201070445_create_line_items.rb
+++ db/migrate/20250201070445_create_line_items.rb
@@ -3,7 +3,7 @@ class CreateLineItems < ActiveRecord::Migration[8.0]
     create_table :line_items do |t|
       t.references :product, null: false, foreign_key: true
       t.references :cart, null: false, foreign_key: true
-      t.integer :quantity
+      t.integer :quantity, default: 1
 
       t.timestamps
     end
