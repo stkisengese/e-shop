ef50a7a92b2947cef7cc93e0b65ac99dd23693d1
stkisengese <skisengese@outlook.com>
2025-02-01 19:42:13 +0300

    fix: fix devise paths

--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,9 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
   # config/routes.rb
-
-  root "products#index"
   
   devise_for :users, controllers: {
-    registrations: 'users/registrations'
+    registrations: 'registrations'
   }
 
   resource :cart, only: [:show, :destroy] do
@@ -24,5 +22,5 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "products#index"
 end
