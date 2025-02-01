7fe55b44de693e2128afe88bccb0172a702de55f
stkisengese <skisengese@outlook.com>
2025-02-01 08:21:02 +0300

    feat: add test data directly

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,8 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+user1 = User.create!(name: "John Kaka", email: "john@example.com", password: "password")
+user2 = User.create!(name: "Jane Dada", email: "jane@example.com", password: "password")
+
+user1.products.create!(title: "Vintage Camera", price: 150.00, model: "Canon AE-1", description: "A classic film camera.", brand: "Canon", color: "Black", condition: "Used")
+user2.products.create!(title: "Leather Jacket", price: 200.00, model: "M-65", description: "A stylish leather jacket.", brand: "Alpha Industries", color: "Brown", condition: "New")
