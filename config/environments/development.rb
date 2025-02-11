a9b3efa4f6460cc9622cf366166de5c7f816f330
Stephen Kisengese <skisengese@gmail.com>
2025-02-11 20:27:12 +0000

    feat: add dev environment path for hosting

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,6 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  config.hosts << "fuzzy-giggle-5gv54p4r6prxf9jj-3000.app.github.dev"
 
   # Make code changes take effect immediately without server restart.
   config.enable_reloading = true
