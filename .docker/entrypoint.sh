ebc00718696427dff239bf3778e4ca24e2addff9
skisenge <skisengese@gmail.com>
2025-02-13 12:57:40 +0300

    fix: allow database migrations

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -4,5 +4,13 @@ set -e
 # Remove a potentially pre-existing server.pid for Rails
 rm -f /rails/tmp/pids/server.pid
 
+# Migrate the database (only if there are pending migrations)
+echo "Running database migrations..."
+ruby/rails db:migrate
+
+# Seed the database (this can always be safely run)
+echo "Seeding the database..."
+ruby/rails db:seed
+
 # Then exec the container's main process (what's set as CMD in the Dockerfile)
 exec "$@"
