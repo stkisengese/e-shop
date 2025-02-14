6783fade6e0c5aa9163e3918a43bd0b67172673e
skisenge <skisengese@gmail.com>
2025-02-15 01:51:14 +0300

    fix: fix carts instance methods

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,38 +1,30 @@
 class CartsController < ApplicationController
-    before_action :authenticate_user!, only: [:show]
-  
-    def show
-      # The @cart is already set by the CurrentCart concern
-    end
-  
-    def add_item
-      product = Product.find(params[:product_id])
-      @cart.add_product(product)
-      redirect_to cart_path, notice: "Product added to cart."
-    end
-  
-    def remove_item
-      product = Product.find(params[:product_id])
-      @cart.line_items.find_by(product: product).destroy
-      redirect_to cart_path, notice: "Product removed from cart."
-    end
-  
-    def empty_cart
-      @cart.line_items.destroy_all
-      redirect_to root_path, notice: "Cart emptied."
-    end
+  before_action :authenticate_user!, only: [:show]
 
-    def update_quantity
-      product = Product.find(params[:product_id])
-      case params[:change]
-      when 'increase'
-        @cart.add_item(product)
-      when 'decrease'
-        @cart.decrease_quantity(product)
-      end
-      
-      respond_to do |format|
-        format.html { redirect_to cart_path, notice: 'Cart updated' }
-        format.turbo_stream
-      end
+  def show
+    # The @cart is already set by the CurrentCart concern
   end
+
+  def add_item
+    product = Product.find(params[:product_id])
+    @cart.add_product(product)
+    redirect_to cart_path, notice: "Product added to cart."
+  end
+
+  def remove_item
+    product = Product.find(params[:product_id])
+    @cart.line_items.find_by(product: product).destroy
+    redirect_to cart_path, notice: "Product removed from cart."
+  end
+
+  def empty_cart
+    @cart.line_items.destroy_all
+    redirect_to root_path, notice: "Cart emptied."
+  end
+
+  def decrease_quantity
+    product = Product.find(params[:product_id])
+    @cart.decrease_quantity(product)
+    redirect_to cart_path, notice: 'Cart updated'
+  end
+end
