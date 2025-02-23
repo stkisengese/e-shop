4a0c9b4c9e3242d094ce58b817880bdcc75c28b2
skisenge <skisengese@gmail.com>
2025-02-23 12:30:09 +0300

    feat: add validation on quantity

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,6 +1,12 @@
 class LineItem < ApplicationRecord
   belongs_to :product
   belongs_to :cart
+  
+  MAX_QUANTITY = 99
+  validates :quantity, presence: true, 
+  numericality: { greater_than: 0, 
+                less_than_or_equal_to: MAX_QUANTITY, 
+                only_integer: true }
 
   def total_price
     product.price * quantity
