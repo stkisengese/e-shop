00cf016db0e657e90fd9772e5b063b979a4e79c7
stkisengese <skisengese@outlook.com>
2025-02-25 06:00:16 +0300

    feat: add render_404 method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,17 @@
 class ApplicationController < ActionController::Base
   include CurrentCart
   before_action :set_cart
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  # Rescues RecordNotFound and RoutingError globally
+  rescue_from ActiveRecord::RecordNotFound, with: :render_404
+  rescue_from ActionController::RoutingError, with: :render_404
+
+  def render_404
+    logger.error "Routing error: #{request.path} not found."
+    respond_to do |format|
+      format.html { render template: 'errors/not_found', status: 404 }
+      format.all { head :not_found }
+    end
+  end
 end
